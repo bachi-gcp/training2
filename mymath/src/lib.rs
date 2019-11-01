8da4af1500c84a13ee0fb3190b3c4f94cda110c7
rohitkasyap <rohitk@biarca.com>
2019-11-01 12:59:17 +0530

    added sub function

--- mymath/src/lib.rs
+++ mymath/src/lib.rs
@@ -1,4 +1,8 @@
 pub mod calci {
+    pub fn sub(a:i32 , b:i32) ->i32 
+{
+a - b
+}
     pub fn add(a: i32, b: i32) -> i32 {
         a + b
     }
--- src/main.rs
+++ src/main.rs
@@ -6,4 +6,8 @@ fn main() {
     let result_add: i32 = calci::add(3,4);
 
     println!("Add result: {}", result_add);
+
+    let result_sub:i32=calci::sub(3,4);
+
+    println!("sub result : {}", result_sub);
 }
