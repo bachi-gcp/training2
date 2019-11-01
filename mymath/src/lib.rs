508eb5a17bc402157850f4d26aa8f8d713c7157a
mugalwarkanchan <mugalwarkanchan@gmail.com>
2019-11-01 12:37:50 +0530

    program changed

--- mymath/src/lib.rs
+++ mymath/src/lib.rs
@@ -2,8 +2,10 @@ pub mod calci {
     pub fn add(a: i32, b: i32) -> i32 {
         a + b
     }
-    pub fn div(c:i32,d:i32)->i32
-    {
+    pub fn div(c: i32,d: i32) -> i32{
         c/d
+    }
+    pub fn modulus(k:i32,j:i32) -> i32{
+     k%j
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use mymath::calci;
 fn main() {
     let result_add: i32 = calci::add(3,4);
 
-    println!("Add result: {}", result_add);
-    println!("div :{}",calci::div(10,2));
+    println!("Add result: {} ", result_add);
+    println!("div :{} ",calci::div(10,2));
+    println!("modulus:{} ",calci::modulus(8,3));
 }
