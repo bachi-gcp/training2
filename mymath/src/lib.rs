fca15388c34db46d19b5812a537aee1da388ed1f
ramreddyamanaganti <ramreddya@biarca.com>
2019-11-04 10:09:56 +0530

    Formatting the text

--- mymath/src/lib.rs
+++ mymath/src/lib.rs
@@ -2,12 +2,11 @@ pub mod calci {
     pub fn add(a: i32, b: i32) -> i32 {
         a + b
     }
-    pub fn mul(a:i32,b:i32)->i32{
-        a*b
+    pub fn mul(a: i32,b: i32) -> i32 {
+        a * b
     }
-    pub fn equal(a:i32,b:i32)->bool{
-      a==b
-
+    pub fn equal(a:i32,b:i32) -> bool {
+        a == b
     }
 
 }
--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,6 @@ fn main() {
     let result_add: i32 = calci::add(3,4);
 
     println!("Add result: {}", result_add);
-    println!("mul result: {}",calci::mul(3,4) );
-    println!("mul result: {}",calci::equal(4,4) );
+    println!("mul result: {}", calci::mul(3,4));
+    println!("mul result: {}", calci::equal(4,4));
 }
